7c1ed33819d0225b70b03a367cfb7df3c6b2ea1c
Ce Wang <7344724+HaloWang@users.noreply.github.com>
2025-10-04 17:53:25 +0800

    code cleanup

--- android/app/src/main/kotlin/com/rwkvzone/chat/MainActivity.kt
+++ android/app/src/main/kotlin/com/rwkvzone/chat/MainActivity.kt
@@ -1,32 +1,34 @@
 package com.rwkvzone.chat
 
+import android.content.Intent
+import android.util.Log
+import androidx.core.content.FileProvider
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
-import android.provider.Settings
-import android.util.Log
 import java.io.File
-import androidx.core.content.FileProvider
 
 class MainActivity : FlutterActivity() {
-    private val CHANNEL = "utils"
+    private val channelName = "utils"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            if (call.method == "installApk") {
-                val path = call.argument<String>("path")
-                if (path != null) {
-                    installApk(path)
-                    result.success(null)
-                } else {
-                    result.error("INVALID_PATH", "Path cannot be null", null)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger, channelName
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "installApk" -> {
+                    val path = call.argument<String>("path")
+                    if (path != null) {
+                        installApk(path)
+                        result.success(null)
+                    } else {
+                        result.error("INVALID_PATH", "Path cannot be null", null)
+                    }
                 }
-            } else {
-                result.notImplemented()
+                else -> {
+                    result.notImplemented()
+                }
             }
         }
     }
@@ -38,32 +40,15 @@ class MainActivity : FlutterActivity() {
             return
         }
 
-        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            FileProvider.getUriForFile(
-                this,
-                "${applicationContext.packageName}.fileprovider",
-                file
-            )
-        } else {
-            Uri.fromFile(file)
-        }
+        val uri = FileProvider.getUriForFile(
+            this, "${applicationContext.packageName}.fileprovider", file
+        )
 
         val intent = Intent(Intent.ACTION_VIEW).apply {
             setDataAndType(uri, "application/vnd.android.package-archive")
             addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            if (!packageManager.canRequestPackageInstalls()) {
-//                val packageUri = Uri.parse("package:${packageName}")
-//                startActivity(Intent(
-//                    Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES,
-//                    packageUri
-//                ))
-//                return
-//            }
-//        }
 
         startActivity(intent)
     }
