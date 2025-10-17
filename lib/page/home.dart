a20316599b4c5c8af225a1ee01ac1dba3d63b9b9
Ce Wang <7344724+HaloWang@users.noreply.github.com>
2025-10-17 15:32:06 +0800

    chore: code optimization

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -15,48 +15,14 @@ import 'package:zone/widgets/model_selector.dart';
 class PageHome extends ConsumerWidget {
   const PageHome({super.key});
 
-  void onNekoTap(BuildContext context) async {
-    final current = P.rwkv.currentModel.q;
-    if (current == null || !current.isNeko) {
-      final nekoList = P.fileManager.getNekoModel();
-      final downloaded = nekoList.where((e) => P.fileManager.locals(e).q.hasFile).toList();
-      if (downloaded.isNotEmpty) {
-        final loaded = await _ModelLoadingDialog.show(context, downloaded.first);
-        if (!loaded) return;
-      } else if (nekoList.isNotEmpty) {
-        Alert.warning(S.current.chat_you_need_download_model_if_you_want_to_use_it);
-        ModelSelector.show(showNeko: true);
-        return;
-      } else {
-        Alert.error('Neko is not available');
-        return;
-      }
-    }
-    P.chat.startNewChat();
-    push(PageKey.neko);
-  }
-
-  void _onChatTap() async {
-    P.chat.startNewChat();
-    push(PageKey.chat);
-  }
-
-  void _onTtsTap() async {
-    P.chat.startNewChat();
-    push(PageKey.talk);
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final version = ref.watch(P.app.version);
-    final isDesktop = ref.watch(P.app.isDesktop);
 
     final width = MediaQuery.of(context).size.width;
 
     final isLandscape = width > 600;
     final maxWidth = width / (isLandscape ? 3 : 2) - (isLandscape ? 60 : 24);
-
-    final s = S.of(context);
 
     return AppScaffold(
       body: SingleChildScrollView(
@@ -97,66 +63,28 @@ class PageHome extends ConsumerWidget {
                 runSpacing: 12,
                 alignment: WrapAlignment.start,
                 runAlignment: WrapAlignment.center,
-                children:
-                    [
-                          buildButton(
-                            title: s.chat,
-                            subtitle: s.chat_with_rwkv_model,
-                            onTap: _onChatTap,
-                            iconColor: Colors.blueAccent,
-                            iconData: FontAwesomeIcons.comments,
-                          ),
-                          buildButton(
-                            title: s.tts,
-                            subtitle: s.tts_detail,
-                            onTap: _onTtsTap,
-                            icon: Container(
-                              height: 48,
-                              width: 48,
-                              alignment: Alignment.center,
-                              decoration: const BoxDecoration(
-                                color: Colors.orange,
-                                shape: BoxShape.circle,
-                              ),
-                              child: const Icon(
-                                Icons.record_voice_over,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                          buildButton(
-                            title: s.neko,
-                            subtitle: s.nyan_nyan,
-                            onTap: () => onNekoTap(context),
-                            iconColor: Colors.pinkAccent,
-                            iconData: FontAwesomeIcons.cat,
-                          ),
-                          buildButton(
-                            title: s.completion_mode,
-                            subtitle: s.text_completion_mode,
-                            onTap: () {
-                              push(PageKey.completion);
-                            },
-                            iconColor: Colors.lightGreen,
-                            iconData: FontAwesomeIcons.feather,
-                          ),
-                          buildButton(
-                            title: isDesktop ? s.offline_translator_server : s.offline_translator,
-                            subtitle: s.offline_translator_detail,
-                            onTap: () {
-                              push(PageKey.translator);
-                            },
-                            iconColor: Colors.blue,
-                            iconData: Icons.translate,
-                          ),
-                        ]
-                        .map(
-                          (e) => ConstrainedBox(
-                            constraints: BoxConstraints(maxWidth: maxWidth),
-                            child: e,
-                          ),
-                        )
-                        .toList(),
+                children: [
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: maxWidth),
+                    child: _ChatButton(),
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: maxWidth),
+                    child: _TTSButton(),
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: maxWidth),
+                    child: _NekoButton(),
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: maxWidth),
+                    child: _CompletionButton(),
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: maxWidth),
+                    child: _TranslatorButton(),
+                  ),
+                ],
               ),
             ),
             const SizedBox(height: 20),
@@ -165,51 +93,253 @@ class PageHome extends ConsumerWidget {
       ),
     );
   }
+}
 
-  Widget buildButton({
-    String? title,
-    String? subtitle,
-    VoidCallback? onTap,
-    Color? iconColor,
-    IconData? iconData,
-    Widget? icon,
-  }) {
+class _ChatButton extends ConsumerWidget {
+  const _ChatButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final s = S.of(context);
+
     return Material(
       clipBehavior: Clip.antiAlias,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: InkWell(
-        onTap: onTap,
+        onTap: () {
+          P.chat.startNewChat();
+          push(PageKey.chat);
+        },
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              if (iconData != null)
-                Align(
-                  alignment: Alignment.topLeft,
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  height: 48,
+                  width: 48,
+                  decoration: const BoxDecoration(
+                    color: Colors.blueAccent,
+                    shape: BoxShape.circle,
+                  ),
+                  alignment: Alignment.center,
+                  child: const FaIcon(FontAwesomeIcons.comments, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(s.chat, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 8),
+              Text(s.chat_with_rwkv_model, style: const TextStyle(fontSize: 12, color: Colors.grey)),
+              const SizedBox(height: 6),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TTSButton extends ConsumerWidget {
+  const _TTSButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final s = S.of(context);
+
+    return Material(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: InkWell(
+        onTap: () {
+          P.chat.startNewChat();
+          push(PageKey.talk);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                height: 48,
+                child: Align(
+                  alignment: Alignment.centerLeft,
                   child: Container(
                     height: 48,
                     width: 48,
-                    decoration: BoxDecoration(
-                      color: iconColor,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      color: Colors.orange,
                       shape: BoxShape.circle,
                     ),
-                    alignment: Alignment.center,
-                    child: FaIcon(iconData, color: Colors.white),
+                    child: const Icon(
+                      Icons.record_voice_over,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              if (icon != null)
-                SizedBox(
+              ),
+              const SizedBox(height: 12),
+              Text(s.tts, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 8),
+              Text(s.tts_detail, style: const TextStyle(fontSize: 12, color: Colors.grey)),
+              const SizedBox(height: 6),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NekoButton extends ConsumerWidget {
+  const _NekoButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final s = S.of(context);
+
+    return Material(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: InkWell(
+        onTap: () async {
+          final current = P.rwkv.currentModel.q;
+          if (current == null || !current.isNeko) {
+            final nekoList = P.fileManager.getNekoModel();
+            final downloaded = nekoList.where((e) => P.fileManager.locals(e).q.hasFile).toList();
+            if (downloaded.isNotEmpty) {
+              final loaded = await _ModelLoadingDialog.show(context, downloaded.first);
+              if (!loaded) return;
+            } else if (nekoList.isNotEmpty) {
+              Alert.warning(S.current.chat_you_need_download_model_if_you_want_to_use_it);
+              ModelSelector.show(showNeko: true);
+              return;
+            } else {
+              Alert.error('Neko is not available');
+              return;
+            }
+          }
+          P.chat.startNewChat();
+          push(PageKey.neko);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
                   height: 48,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: icon,
+                  width: 48,
+                  decoration: const BoxDecoration(
+                    color: Colors.pinkAccent,
+                    shape: BoxShape.circle,
                   ),
+                  alignment: Alignment.center,
+                  child: const FaIcon(FontAwesomeIcons.cat, color: Colors.white),
                 ),
-              if (title != null) const SizedBox(height: 12),
-              if (title != null) Text(title, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-              if (subtitle != null) const SizedBox(height: 8),
-              if (subtitle != null) Text(subtitle, style: const TextStyle(fontSize: 12, color: Colors.grey)),
+              ),
+              const SizedBox(height: 12),
+              Text(s.neko, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 8),
+              Text(s.nyan_nyan, style: const TextStyle(fontSize: 12, color: Colors.grey)),
+              const SizedBox(height: 6),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CompletionButton extends ConsumerWidget {
+  const _CompletionButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final s = S.of(context);
+
+    return Material(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: InkWell(
+        onTap: () {
+          push(PageKey.completion);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  height: 48,
+                  width: 48,
+                  decoration: const BoxDecoration(
+                    color: Colors.lightGreen,
+                    shape: BoxShape.circle,
+                  ),
+                  alignment: Alignment.center,
+                  child: const FaIcon(FontAwesomeIcons.feather, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(s.completion_mode, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 8),
+              Text(s.text_completion_mode, style: const TextStyle(fontSize: 12, color: Colors.grey)),
+              const SizedBox(height: 6),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TranslatorButton extends ConsumerWidget {
+  const _TranslatorButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final s = S.of(context);
+    final isDesktop = ref.watch(P.app.isDesktop);
+
+    return Material(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: InkWell(
+        onTap: () {
+          push(PageKey.translator);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  height: 48,
+                  width: 48,
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                  ),
+                  alignment: Alignment.center,
+                  child: const Icon(Icons.translate, color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                isDesktop ? s.offline_translator_server : s.offline_translator,
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              Text(s.offline_translator_detail, style: const TextStyle(fontSize: 12, color: Colors.grey)),
               const SizedBox(height: 6),
             ],
           ),
